c2880e6a28b1f77a38f1869e7c1a36cf8031d500
Steve <lysoll8444@gmail.com>
2023-07-27 17:32:04 -0500

    exercise-five

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,9 @@
-const sumAll = function() {
-
+const sumAll = function(firstNum, lastNum) {
+	sum = 0
+	for ( i = firstNum; firstNum <= lastNum; firstNum++){
+		sum += firstNum;
+	}
+	return sum;
 };
 
 // Do not edit below this line
