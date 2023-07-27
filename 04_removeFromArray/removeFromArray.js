f401d6a4bc81c004acfad5adcfb708fc93b6fb5e
Steve <lysoll8444@gmail.com>
2023-07-27 17:26:45 -0500

    exercise-four

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, removeNum) {
+	for (i in array){
+		if (array[i] == removeNum){
+			array.splice(i, 1);
+		}
+	}
+	return array;
 };
 
 // Do not edit below this line
