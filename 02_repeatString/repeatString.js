2a7169ff24175cd7022d62dba91e19e999fc30d1
Steve <lysoll8444@gmail.com>
2023-07-27 17:10:58 -0500

    exercise-two

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+	let wordCount = "";
+	for ( let i = 0; i < num; i++){
+		wordCount += string;
+	}
+	return wordCount;
 };
 
 // Do not edit below this line
