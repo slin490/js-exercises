598d3fabaf1bf35ab2e5d71c815953fe7923c473
Steve <lysoll8444@gmail.com>
2023-07-27 17:16:04 -0500

    exercise-three

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,6 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+	newString = string.split("").reverse().join('');
+	return newString;
 };
 
 // Do not edit below this line
